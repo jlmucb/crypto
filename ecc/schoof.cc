42513c84a652389d24f8ce16ff4b27092d5d29f4
John Manferdelli <johnmanferdelli@hotmail.com>
2015-01-11 18:57:24 -0800

    more

--- ecc/schoof.cc
+++ ecc/schoof.cc
@@ -5,7 +5,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //     http://www.apache.org/licenses/LICENSE-2.0
-// or in the the file LICENSE-2.0.txt in the top level sourcedirectory
+// or in the the file LICENSE-2.0.txt in the top level source directory
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
