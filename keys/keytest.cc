34f629d4ac689d91515e1ef9c6b6354807e861f1
John Manferdelli <jlm@Sylow.local>
2016-07-14 20:05:10 -0700

    another test fix

--- keys/keytest.cc
+++ keys/keytest.cc
@@ -11,7 +11,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License
-// Project: New Cloudproxy Crypto
 // File: keytest.cc
 
 #include "cryptotypes.h"
@@ -27,6 +26,10 @@
 #include "string.h"
 #include "conversions.h"
 #include <cmath>
+
+
+DEFINE_bool(printall, false, "printall flag");
+
 
 class KeyTest : public ::testing::Test {
  protected:
@@ -47,9 +50,12 @@ bool simpletest1() {
     printf("GenerateAesKey failed\n");
     return false;
   }
-  printf("Original key:\n");
-  the_key.PrintKey();
-  printf("\n");
+
+  if (FLAGS_printall) {
+    printf("Original key:\n");
+    the_key.PrintKey();
+    printf("\n");
+  }
 
   string filename("jlmTestSave1");
   if (!((CryptoKey*)&the_key)->SaveKey(filename)) {
@@ -61,9 +67,35 @@ bool simpletest1() {
     printf("ReadKey failed\n");
     return false;
   }
-  printf("Recovered key:\n");
-  new_key.PrintKey();
-  printf("\n");
+  if (FLAGS_printall) {
+    printf("Recovered key:\n");
+    new_key.PrintKey();
+    printf("\n");
+  }
+  if (the_key.key_name_ != nullptr && *the_key.key_name_ != *new_key.key_name_) {
+    printf("key names disagree\n");
+    return false;
+  }
+  if (the_key.key_type_ != nullptr && *the_key.key_type_ != *new_key.key_type_) {
+    printf("key types disagree\n");
+    return false;
+  }
+  if (the_key.key_usage_ != nullptr && *the_key.key_usage_ != *new_key.key_usage_) {
+    printf("key usage disagree\n");
+    return false;
+  }
+  if (*the_key.symmetric_algorithm_type_ != *new_key.symmetric_algorithm_type_) {
+    printf("Algorithm types disagree\n");
+    return false;
+  }
+  if (the_key.symmetric_key_bit_size_ != new_key.symmetric_key_bit_size_) {
+    printf("key sizes differ\n");
+    return false;
+  }
+  if (memcmp(the_key.symmetric_key_bytes_ , new_key.symmetric_key_bytes_, the_key.symmetric_key_bit_size_ / 8) !=0) {
+    printf("key values differ\n");
+    return false;
+  }
   return true;
 }
 
@@ -78,11 +110,11 @@ bool RunTestSuite() {
   }
 
   if (!key_store.ReadStore("TestKeyStore")) {
-    printf("Cant read key store\n");
+    printf("Can't read key store\n");
     return false;
   }
   if (!key_store.AddKey((CryptoKey*)&the_key)) {
-    printf("Cant add to key store\n");
+    printf("Can't add to key store\n");
     return false;
   }
 
@@ -92,18 +124,24 @@ bool RunTestSuite() {
     printf("Cant find key in store\n");
     return false;
   }
-  ((SymmetricKey*)p_msg)->PrintKey();
+  if (FLAGS_printall) {
+    ((SymmetricKey*)p_msg)->PrintKey();
+  }
   return true;
 }
 
 TEST(FirstKeyCase, FirstKeyTest) { EXPECT_TRUE(simpletest1()); }
-
 TEST_F(KeyTest, RunTestSuite) { EXPECT_TRUE(RunTestSuite()); }
 
 DEFINE_string(log_file, "keytest.log", "keytest logging file name");
 
 int main(int an, char** av) {
   ::testing::InitGoogleTest(&an, av);
+#ifdef __linux__
+  gflags::ParseCommandLineFlags(&an, &av, true);
+#else
+  google::ParseCommandLineFlags(&an, &av, true);
+#endif
   if (!InitUtilities(FLAGS_log_file.c_str())) {
     printf("InitUtilities() failed\n");
     return 1;
