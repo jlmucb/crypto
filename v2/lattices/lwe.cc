635ab18d4fbfb567f22242626a117dd663a76b35
John Manferdelli <johnmanferdelli@hotmail.com>
2020-09-21 12:26:02 -0700

    lwe stuff

--- v2/lattices/lwe.cc
+++ v2/lattices/lwe.cc
@@ -1,5 +1,4 @@
 // Copyright 2020 John Manferdelli, All Rights Reserved.
-//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
@@ -14,17 +13,42 @@
 
 #include "crypto_support.h"
 #include "lattice.h"
+#include "big_num.h"
+#include "big_num_functions.h"
 
 // m, n, q, m >=n,  chi is error dist, s.  M= {0,1}^l
-// C = Zq^n Zq^l
+// C = Zq^n x Zq^l
 // S in Zq^(m x l), A in Zq^(m x n), E in Zq^(m x l).
 // E is chosen from chi.
 // P = AS+E
 // Encrypt:
 //    v in {0,1}^l, a in {0,1}^m (random)
-//    (u=A^Ta, P^Ta+close(q/2)v)
+//    (u=A^Ta, c = P^Ta+close(q/2)v)
 //  Decrypt
 //    D = close(close(q/2)^(-1)) (c - S^Tu) mod 2
 class lwe {
 public:
+  int l_;
+  int m_;
+  int n_;
+  big_num q_;
+  big_num* A_;
+  big_num* S_;
+  big_num* E_;
+
+  lwe();
+  ~lwe();
+
+  bool init(int l, int m, int n, big_num& q, double s_param);
+  bool encrypt(int size_in, byte* in, big_num* out1, big_num* out2);
+  bool decrypt(big_num& in1, big_num& in2, int* size_out, byte* out);
 };
+
+bool random_from_q(big_num& q, big_num* out) {
+  return true;
+}
+
+bool random_from_chi(big_num& s, big_num* out) {
+  return true;
+}
+
