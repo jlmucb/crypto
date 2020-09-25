c42556abfeee7bb5e737572a5ccf3e731512ff03
John Manferdelli <johnmanferdelli@hotmail.com>
2020-09-24 17:11:30 -0700

    poly tests

--- v2/lattices/test_lattice.cc
+++ v2/lattices/test_lattice.cc
@@ -722,6 +722,43 @@ bool test_poly_support() {
   if (!poly_equal(n, mm_test, poly_5))
     return false;
 
+  if (!poly_mult_mod_poly(n, modulus,  poly_1, poly_1, poly_2))
+  poly_zero(10, poly_4);
+  poly_zero(10, poly_5);
+#if 0
+  if (!poly_euclid(n, modulus, poly_2, poly_1, poly_4, poly_5))
+    return false;
+  if (FLAGS_print_all) {
+    print_poly(n, poly_2); printf(" = ");
+    print_poly(n, poly_1); printf(" * ");
+    print_poly(n, poly_4); printf(" = ");
+    print_poly(n, poly_5); printf("\n");
+  }
+  poly_2[0] = (poly_2[0] + 1LL) % modulus;
+  poly_zero(10, poly_3);
+  poly_zero(10, poly_4);
+  poly_zero(10, poly_5);
+  if (!poly_gcd(n, modulus, poly_1, poly_2, poly_3, poly_4, poly_5))
+    return false;
+  if (FLAGS_print_all) {
+    printf("[ "); print_poly(n, poly_1); printf(" ] ");
+    printf(" [ "); print_poly(n, poly_3); printf(" ] + ");
+    printf("[ "); print_poly(n, poly_2); printf(" ] ");
+    printf(" [ "); print_poly(n, poly_4); printf(" ] = ");
+    printf(" [ "); print_poly(n, poly_5); printf(" ]\n");
+  }
+  poly_zero(10, poly_3);
+  poly_zero(10, poly_4);
+  poly_zero(10, poly_5);
+  if (!poly_inverse_mod_poly(n, modulus, poly_2, poly_1, poly_5))
+    return false;
+  if (FLAGS_print_all) {
+    printf("[ "); print_poly(n, poly_1); printf(" ] ^ (-1) ");
+    printf("(mod  [ "); print_poly(n, poly_3); printf(" ]) = ");
+    printf("[ "); print_poly(n, poly_5); printf(" ]\n");
+  }
+#endif
+
   return true;
 }
 
