a1d01f45113b589346930bf77a4fa428603de121
John Manferdelli <johnmanferdelli@hotmail.com>
2020-09-28 22:47:04 -0700

    first cryptutil

--- v2/cryptutil/cryptutil.cc
+++ v2/cryptutil/cryptutil.cc
@@ -37,7 +37,7 @@
 #include "symmetric_cipher.h"
 
 
-int num_cryptutil_ops = 28;
+int num_cryptutil_ops;
 std::string cryptutil_ops[] = {
     "--operation=tobase64 --direction=[left-right|right-left] " \
     "--input_file=file --output_file=file",
@@ -69,7 +69,7 @@ std::string cryptutil_ops[] = {
     "--input_file=file --output_file=file"
     "\n",
     "--operation=generate_scheme --algorithm=alg --key_name=name " \
-    "--duration=dur --output_file=file",
+    "encrypt_key_size=size --mac_key_size=size --duration=dur --output_file=file",
     "--operation=read_scheme --algorithm=alg --key_name=name" \
     " --duration=dur --input_file=file" \
     "\n",
@@ -88,10 +88,11 @@ std::string cryptutil_ops[] = {
     "--input_file=file --output_file=file",
 };
 
-int num_cryptutil_algs = 23;
+int num_cryptutil_algs;
 std::string cryptalgs[] = {
     "aes", "rsa", "ecc", "sha-1", "sha-256", "sha-3",
-    "hmac-sha-256", "pbdkf", "twofish", "tea", "simon"};
+    "hmac-sha-256", "pbdkf", "twofish", "tea", "simon",
+    "aes-hmac-sha256-ctr", "aes-hmac-sha256-cbc",};
 
 void print_options() {
   printf("Permitted operations:\n\n");
@@ -114,6 +115,8 @@ DEFINE_string(input2_file, "", "Second input file name");
 DEFINE_string(output_file, "", "Output file name");
 DEFINE_string(direction, "left-right",
               "string value direction left-right or right-left");
+DEFINE_int32(encrypt_key_size, 128, "encrypt-key-size-in-bits");
+DEFINE_int32(mac_key_size, 128, "mac-key-size-in-bits");
 DEFINE_string(algorithm, "sha256", "hash algorithm");
 DEFINE_string(duration, "1Y", "duration");
 DEFINE_string(pass, "password", "password");
@@ -148,13 +151,50 @@ int main(int an, char** av) {
     printf("init_crypto failed\n");
     return 1;
   }
+  int ret = 0;
 
   if ("tobase64" == FLAGS_operation) {
-  } else if ("todecimal" == FLAGS_operation) {
-  } else if ("tohex" == FLAGS_operation) {
-  } else if ("fromhex" == FLAGS_operation) {
-  } else if ("fromdecimal" == FLAGS_operation) {
+    file_util in_file;
+    file_util out_file;
+
+    if (!in_file.open(FLAGS_input_file.c_str())) {
+      printf("Can't open %s\n", FLAGS_input_file.c_str());
+      ret = 1;
+      goto done;
+    }
+    int size_in = in_file.bytes_in_file();
+    byte in_buf[size_in];
+    in_file.close();
+    if (in_file.read_file(FLAGS_input_file.c_str(), size_in, in_buf) < size_in) {
+      printf("Can't read %s\n", FLAGS_input_file.c_str());
+      ret = 1;
+      goto done;
+    }
+    string bytes;
+    string base64;
+    bytes.assign((const char*) in_buf, (size_t) size_in);
+    if (!bytes_to_base64(bytes, &base64)) {
+      printf("Can't convert to base64\n");
+      ret = 1;
+      goto done;
+    }
+    if (!out_file.write_file(FLAGS_output_file.c_str(), (int) base64.size(),
+            (byte*) base64.data())) {
+      printf("Can't write %s\n", FLAGS_output_file.c_str());
+      ret = 1;
+      goto done;
+    }
+    goto done;
   } else if ("frombase64" == FLAGS_operation) {
+    // base64_to_bytes(string& b64, string* b);
+  } else if ("todecimal" == FLAGS_operation) {
+    // digit_convert_to_decimal(int size_a, uint64_t* n, string* s);
+  } else if ("fromdecimal" == FLAGS_operation) {
+    // digit_convert_from_decimal(string& s, int size_n, uint64_t* n);
+  } else if ("tohex" == FLAGS_operation) {
+    // bytes_to_hex(string& b, string* h);
+  } else if ("fromhex" == FLAGS_operation) {
+    // hex_to_bytes(string& h, string* b);
   } else if ("hash" == FLAGS_operation) {
   } else if ("encrypt_with_key" == FLAGS_operation) {
     if (FLAGS_algorithm == "aes") {
@@ -175,38 +215,79 @@ int main(int an, char** av) {
       printf("Decrypt: Unknown encryption alg\n");
     }
   } else if ("scheme_decrypt" == FLAGS_operation) {
-    if (FLAGS_algorithm == "aes128-ctr-hmacsha256-sympad") {
+    // encryption_scheme
+    if (FLAGS_algorithm == "aes-hmac-sha256-ctr") {
+    } else if (FLAGS_algorithm == "aes-hmac-sha256-cbc") {
     } else {
       printf("scheme_decrypt: unsupported algorithm %s\n",
               FLAGS_algorithm.c_str());
-      return 1;
+      ret = 1;
+      goto done;
     }
   } else if ("scheme_encrypt" == FLAGS_operation) {
-    if (FLAGS_algorithm == "aes128-cbc-hmacsha256-sympad") {
-    } else if (FLAGS_algorithm == "aes128-ctr-hmacsha256-sympad") {
+    if (FLAGS_algorithm == "aes-hmac-sha256-ctr") {
+
+      // bool init(const char* alg, const char* id_name,
+      // const char* mode, const char* pad, const char* purpose,
+      // const char* not_before, const char* not_after,
+      // const char* enc_alg, int size_enc_key, string& enc_key,
+      // const char* enc_key_name, const char* hmac_alg,
+      // int size_hmac_key,  string& hmac_key, int size_nonce,
+      // string& nonce);
+      // bool encrypt_message(int size_in, byte* in, int size_out, byte* out);
+      // bool decrypt_message(int size_in, byte* in, int size_out, byte* out);
+    } else if (FLAGS_algorithm == "aes-hmac-sha256-cbc") {
     } else {
       printf("scheme_encrypt: unsupported algorithm %s\n",
               FLAGS_algorithm.c_str());
-      return 1;
+      ret = 1;
+      goto done;
     }
   } else if ("generate_scheme" == FLAGS_operation) {
-  } else if ("generate__key" == FLAGS_operation) {
+    // scheme_message* make_scheme(const char* alg, const char* id_name,
+    //   const char* mode, const char* pad, const char* purpose,
+    //   const char* not_before, const char* not_after,
+    //   const char* enc_alg, int size_enc_key, string& enc_key,
+    //   const char* enc_key_name, const char* hmac_alg,
+    //   int size_hmac_key,  string& hmac_key, int size_nonce,
+    //   string& nonce);
+    if (FLAGS_algorithm == "aes-hmac-sha256-ctr") {
+    } else if (FLAGS_algorithm == "aes-hmac-sha256-cbc") {
+    } else {
+      printf("scheme_decrypt: unsupported algorithm %s\n",
+              FLAGS_algorithm.c_str());
+      ret = 1;
+      goto done;
+    }
+  } else if ("read_scheme" == FLAGS_operation) {
+    // scheme_message scheme_msg;
+  } else if ("generate_key" == FLAGS_operation) {
   } else if ("read_key" == FLAGS_operation) {
   } else if ("get_random" == FLAGS_operation) {
-  } else if ("read_scheme" == FLAGS_operation) {
+    // int crypto_get_random_bytes(int num_bytes, byte* buf);
   } else if ("encrypt_with_scheme" == FLAGS_operation) {
   } else if ("decrypt_with_password" == FLAGS_operation) {
   } else if ("pkcs_sign_with_key" == FLAGS_operation) {
   } else if ("pkcs_verify_with_key" == FLAGS_operation) {
+    if (FLAGS_algorithm == "aes-hmac-sha256-ctr") {
+    } else if (FLAGS_algorithm == "aes-hmac-sha256-cbc") {
+    } else {
+      printf("scheme_decrypt: unsupported algorithm %s\n",
+              FLAGS_algorithm.c_str());
+      ret = 1;
+      goto done;
+    }
   } else if ("pkcs_seal_with_key" == FLAGS_operation) {
   } else if ("pkcs_unseal_with_key" == FLAGS_operation) {
   } else if ("sign_digest_with_key" == FLAGS_operation) {
   } else if ("verify_digest_with_key" == FLAGS_operation) {
   } else {
     printf("%s: unsupported operation\n", FLAGS_operation.c_str());
+    ret = 1;
+    goto done;
   }
 
+done:
   close_crypto();
-
-  return 0;
+  return ret;
 }
