68451f113e4db6c1d273a9b3ed4a3665776f5569
John Manferdelli <johnmanferdelli@hotmail.com>
2021-05-05 21:52:20 -0700

    more sha3 analysis

--- v2/hash/sha3_analysis.cc
+++ v2/hash/sha3_analysis.cc
@@ -13,6 +13,11 @@
 // limitations under the License
 // File: sha3_analysis.cc
 
+#include <stdint.h>
+#include <stdio.h>
+
+typedef uint8_t byte;
+
 const int lane_exp = 6;
 
 inline int lane_size(int exp) {
@@ -35,26 +40,24 @@ int index(int size_lane, int x, int y, int z) {     // row, col, lane
   return size_lane * (5 * y + x) + z;
 }
 
-void print_row(int lane_size, int y, int z, byte* s) {
+void print_row(int size_lane, int y, int z, byte* s) {
   printf("row %d %d: ", y, z);
   for (int x= 0; x < 5; x++)
-    printf("%1x", s[index(size_lane, x, y, z)];
+    printf("%1x", s[index(size_lane, x, y, z)]);
   printf("\n");
 }
 
-void print_col(int lane_size, int x, int z, byte* s) {
+void print_col(int size_lane, int x, int z, byte* s) {
   printf("column %d %d: ", x, z);
   for (int y= 0; y < 5; y++)
-    printf("%1x", s[index(size_lane, x, y, z)];
+    printf("%1x", s[index(size_lane, x, y, z)]);
   printf("\n");
 }
 
-void print_lane (int lane_size, int x, int y, byte* s) {
-
-void print_col(int lane_size, int x, int z, byte* s) {
+void print_lane (int size_lane, int x, int y, byte* s) {
   printf("lane %d %d: ", x, y);
-  for (int z= 0; z < lane_size; z++)
-    printf("%1x", s[index(size_lane, x, y, z)];
+  for (int z= 0; z < size_lane; z++)
+    printf("%1x", s[index(size_lane, x, y, z)]);
   printf("\n");
 }
 
@@ -82,43 +85,97 @@ byte column_parities(int size_lane, byte* in_state, int x, int z) {
   int x2 = (x + 1) % 5;
   int z2 = (z + 1) % 5;
   for (int y = 0; y < 4; y++) {
-    parity ^= in_state[index(lane_size, x1, y, z1)] ^ in_state[index(lane_size, x2, y, z2)];
+    parity ^= in_state[index(size_lane, x1, y, z1)] ^ in_state[index(size_lane, x2, y, z2)];
   }
   return parity;
 }
 
 // add parity of two columns to a column
 void theta_f(int size_lane, byte* in_state, byte* out_state) {
-  for (int x = 0; i < 5; x++) {
+  for (int x = 0; x < 5; x++) {
     for (int z = 0; z < size_lane; z++) {
       byte parity = column_parities(size_lane, in_state, x, z);
       for (int y = 0; y < 5; y++)
-        out_state[index(lane_size, x, y, z)] =  in_state[index(lane_size, x, y, z)] ^ parity;
+        out_state[index(size_lane, x, y, z)] =  in_state[index(size_lane, x, y, z)] ^ parity;
     }
   }
 }
 
+void mat_mult(int a11, int a12, int a21, int a22,
+              int b11, int b12, int b21, int b22,
+              int* c11, int* c12, int* c21, int* c22) {
+  *c11 = (a11*b11 + a12*b21) % 5;
+  *c12 = (a11*b12 + a12*b22) % 5;
+  *c21 = (a21*b11 + a22*b21) % 5;
+  *c22 = (a21*b12 + a22*b22) % 5;
+}
+
 // rotate bits in lane by T(x,y)
 void rho_f(int size_lane, byte* in_state, byte* out_state) {
+  int x1, y1;
+  int a11 = 1; int a12 = 0;
+  int a21 = 0; int a22 = 1;
+  int c11, c12, c21, c22;
+  int k;
+
+  for (int z = 0; z < size_lane; z++) {
+    for (int t = 0; t < 24; t++) {
+      mat_mult(a11, a12, a21, a22, 0, 1, 2, 3, &c11, &c12, &c21, &c22);
+      a11 = c11; a12 = c12; a21 = c21; a22 = c22;
+      x1 = a11; y1= (2*a21 + 3*a22) % 5;
+      if (x1 == 0 && y1 == 0)
+        k = z;
+      else
+        k = (z - ((t + 1)*(t + 2)) / 2) % size_lane;
+      out_state[index(size_lane, x1, y1, z)] = in_state[index(size_lane, x1, y1, k)];
+    }
+  }
 }
 
 // reorder lanes
 void pi_f(int size_lane, byte* in_state, byte* out_state) {
+  int x1, y1;
+  for (int x = 0; x < 5; x++) {
+    for (int y = 0; y < 5; y++) {
+      index_pair_transform(x, y, &x1, &y1);
+      for (int z = 0; z < size_lane; z++) {
+        out_state[index(size_lane, x1, y1, z)] =  in_state[index(size_lane, x1, y1, z)];
+      }
+    }
+  }
 }
 
 // non-linear transform of rows
 void chi_f(int size_lane, byte* in_state, byte* out_state) {
+  for (int x = 0; x < 5; x++) {
+    for (int y = 0; y < 5; y++) {
+      for (int z = 0; z < size_lane; z++) {
+        out_state[index(size_lane, x, y, z)] =  in_state[index(size_lane, x, y, z)] ^
+          (in_state[index(size_lane, (x + 1) % 5, y, z)] ^ 1) &
+          in_state[index(size_lane, (x + 2) % 5, y, z)];
+      }
+    }
+  }
 }
 
 // byte rc_t[];  // rc[t] = x^t mod (x^8 + x^6 + x^5 + x^4 + 1) mod 2
-
-inline byte RC(int rnd, int j) {
-  // rc_t[j + 7 * rnd]
+// RC(rnd,0,0,2**j -1] = rc_t[j+7*rnd];
+inline byte RC(int rnd, int x, int y, int z) {
+  // rc_t[j + 7 * rnd], 0 <= j <= lane_exp
+  // all others are 0
   return 0;
 }
 
 // add round constants
-void iota_f(int size_lane, byte* in_state, byte* out_state) {
+void iota_f(int rnd, int size_lane, byte* in_state, byte* out_state) {
+  for (int x = 0; x < 5; x++) {
+    for (int y = 0; y < 5; y++) {
+      for (int z = 0; z < size_lane; z++) {
+        out_state[index(size_lane, x, y, z)] =  in_state[index(size_lane, x, y, z)] ^
+          RC(rnd, x, y, z);
+      }
+    }
+  }
 }
 
 void init_state(int size_lane, byte* state) {
@@ -126,5 +183,7 @@ void init_state(int size_lane, byte* state) {
     state[i] = 0;
 }
 
-
+int main(int an, char** av) {
+  return 0;
+}
 
