c7313619aa9f386dfa099694c638952a47bcfe3d
John Manferdelli <johnmanferdelli@hotmail.com>
2021-05-18 21:04:17 -0700

    Fixed some comments

--- v2/include/hash_drng.h
+++ v2/include/hash_drng.h
@@ -10,7 +10,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License
-// File: entropy_collection.h
+// File: hash_drng.h
 
 #ifndef _CRYPTO_HASH_DRNG_H__
 #define _CRYPTO_HASH_DRNG_H__
--- v2/rng/entropy_accumulate.cc
+++ v2/rng/entropy_accumulate.cc
@@ -9,7 +9,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License
-// File: entropy_collection.cc
+// File: entropy_accumulate.cc
 
 #include <gtest/gtest.h>
 #include <gflags/gflags.h>
